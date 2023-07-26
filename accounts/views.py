2c8c29fd6537659f8334f36e5c0fe6f88938b54e
BM Hasanuzzaman <47479370+BMHasanuzzaman@users.noreply.github.com>
2023-07-26 16:05:54 +0600

    new

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+# accounts/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')  # Change 'home' to the URL name of your home page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
---
+++ myproject/urls.py
@@ -0,0 +1,11 @@
+# myproject/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views  # Import the views module from the main project folder
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homepage, name='home'),  # Make sure the URL pattern is named 'home'
+    path('accounts/', include('accounts.urls')),
+]
